91d9ed236cfbd43f2844f25887f18d60ec737020
CulleyS <culley.smith@gmail.com>
2014-03-17 20:49:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StompFish::Application.config.secret_key_base = 'fdf6a226bf30c0187a52a2b1ddb4ed245da3749f2a8ad35dd77125acc913659ef19acaca45c809d4a45da22c9ba271f12f47f7260e99abed95132d1b9a5de299'
