9a7726e7fd445297091e113d7fe23eb544897af7
CulleyS <culley.smith@gmail.com>
2014-03-18 21:38:09 -0500

    add CatalogImport::CreateSongFile

---
+++ lib/stomp_fish/catalog_import/create_song_file.rb
@@ -0,0 +1,27 @@
+module StompFish
+  module CatalogImport
+    class CreateSongFile
+      attr_reader :tags, :song_id, :song_file_model
+
+      def initialize(tags, song_id, options = {})
+        @tags = tags
+        @song_id = song_id
+        @song_file_model = options[:song_file_model] || SongFileModel
+      end
+
+      def add
+        sf = song_file_model.find_or_create_by(filename: tags[:filename])
+        sf.update(bitrate: tags[:bitrate],
+                  duration: tags[:duration],
+                  filesize: tags[:filesize],
+                  format: tags[:format],
+                  fileable_id: song_id,
+                  fileable_type: "Song")
+      end
+
+      def self.add(tags, song_id, options = {})
+        new(tags, song_id, options).add
+      end
+    end
+  end
+end
---
+++ spec/lib/stomp_fish/catalog_import/create_song_file_spec.rb
@@ -0,0 +1,36 @@
+require 'stomp_fish/catalog_import/create_song_file'
+
+module StompFish
+  module CatalogImport
+    describe CreateSongFile do
+      let(:tags) do 
+        {filename: "filename", bitrate: "bitrate",
+         duration: "duration", format: "format",
+         filesize: "filesize"}
+      end
+
+      let(:song_file_model) { Class.new }
+      let(:song_instance) { double(:update) }
+
+      it "adds a SongFile" do
+        stub_const("SongFileModel", song_file_model)
+
+        expect(song_file_model).
+          to receive(:find_or_create_by).
+          with(filename: "filename").
+          and_return(song_instance)
+
+        expect(song_instance).
+          to receive(:update).
+          with(filesize: "filesize",
+               bitrate: "bitrate",
+               format: "format",
+               duration: "duration",
+               fileable_id: 1,
+               fileable_type: "Song")
+
+          CreateSongFile.add(tags, 1, song_file_model: song_file_model)
+      end
+    end
+  end
+end
