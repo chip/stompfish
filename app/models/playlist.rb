42e45dee0d3a306d985a60eaa3cb95db308bb789
CulleyS <culley.smith@gmail.com>
2014-03-22 12:56:52 -0500

    add #runtime to Playlist to find total playtime

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,13 @@
 class Playlist < ActiveRecord::Base
   has_and_belongs_to_many :songs
   validates_presence_of :title
+
+  def runtime
+    SongFileFormatters::DurationFormatter.as_strftime(runtime_as_integer)
+  end
+
+  private
+  def runtime_as_integer
+    songs.map(&:duration).inject(:+)
+  end
 end
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -8,4 +8,23 @@ describe Playlist do
   context "validations" do
     it { should validate_presence_of(:title) }
   end
+
+  context "#runtime" do
+    it "calculates total runtime of playlist" do
+      song_one = Song.create(title: "One", artist_id: 1, album_id: 2)
+      song_two = Song.create(title: "Two", artist_id: 1, album_id: 2)
+
+      song_file_one = SongFile.create(filename: "One", fileable_id: song_one.id,
+                                      fileable_type: "Song", duration: 123)
+
+      song_file_two = SongFile.create(filename: "Two", fileable_id: song_two.id,
+                                      fileable_type: "Song", duration: 345)
+
+      playlist = Playlist.new(title: "Playlist")
+      playlist.songs = [song_one, song_two]
+      playlist.save
+
+      expect(playlist.runtime).to eq("07:48")
+    end
+  end
 end
