efc55ec617e248760bffc218cc18aa40b7a0ce22
CulleyS <culley.smith@gmail.com>
2014-03-23 17:36:40 -0500

    add documentation to/organize PlaylistCollaborator model

--- app/models/playlist_collaborator.rb
+++ app/models/playlist_collaborator.rb
@@ -1,7 +1,9 @@
 class PlaylistCollaborator < ActiveRecord::Base
+  # associations
   belongs_to :playlist
   belongs_to :song
 
+  # validations
   validates_presence_of :position, :playlist_id, :song_id
   validates_uniqueness_of :position, scope: :playlist_id
 end
