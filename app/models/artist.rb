6ce416e3527e93bac52b8416e9aaab191b139e97
CulleyS <culley.smith@gmail.com>
2014-03-23 17:33:49 -0500

    add documentation to/organize Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,8 +3,10 @@ require 'elasticsearch/model'
 class Artist < ActiveRecord::Base
   include Elasticsearch::Model
 
+  # associations
   has_many :albums
   has_many :songs
 
+  # validations
   validates :name, presence: true, uniqueness: true
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe Artist do
-  it { should have_many(:albums) }
-  it { should have_many(:songs) }
+  context "associations" do
+    it { should have_many(:albums) }
+    it { should have_many(:songs) }
+  end
 
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
+  context "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+  end
 end
