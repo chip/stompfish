806e9d22578c5a3cb8cdca0894666b824fff01cd
CulleyS <culley.smith@gmail.com>
2014-03-24 18:18:32 -0500

    add Song.search to songs#index/JSON rendering to songs#index

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,15 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:query]
+      @songs = Song.search(params[:query])
+    else
+      @songs = Song.all
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @songs }
+    end
   end
 
   def show
--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -17,6 +17,19 @@ describe SongsController do
     it "renders the :index view" do
       expect(response).to render_template :index
     end
+
+    it "renders @songs as json" do
+      get :index, format: :json
+      songs = Song.all
+      expect(response.body).to eq(songs.to_json)
+    end
+
+    context "with valid search term" do
+      it "assigns Song.search to @songs" do
+        expect(Song).to receive(:search).with("some")
+        get :index, query: "some"
+      end
+    end
   end
 
   describe "GET #show" do
