44141a20f27a4806d65bb775a1c148a8e6c75f75
CulleyS <culley.smith@gmail.com>
2014-03-24 23:04:41 -0500

    add caching to ReleaseDateSerializer

--- app/serializers/genre_serializer.rb
+++ app/serializers/genre_serializer.rb
@@ -1,6 +1,8 @@
 class GenreSerializer < ActiveModel::Serializer
+  # cache
   cached
   delegate :cache_key, to: :object
 
+  # associations
   attributes :name
 end
--- app/serializers/release_date_serializer.rb
+++ app/serializers/release_date_serializer.rb
@@ -1,3 +1,8 @@
 class ReleaseDateSerializer < ActiveModel::Serializer
+  # cache
+  cached
+  delegate :cache_key, to: :object
+
+  # attributes
   attributes :year
 end
