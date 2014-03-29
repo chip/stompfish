a4f6f177b5ee8774ee1622a5e30cf67d1f85e038
CulleyS <culley.smith@gmail.com>
2014-03-29 00:58:29 -0500

    add basic working Monitor initializer to import new files automatically

--- config/initializers/monitor.rb
+++ config/initializers/monitor.rb
@@ -1,3 +1,14 @@
-Catalog::Import::Monitor.listen("/mnt/music", ignore: "/mnt/music/TMP") do |event|
-  Catalog::Import::RecursiveImport.scan(event)
+Catalog::Import::Monitor.listen("/mnt/music/TMP") do |event|
+  sleep 3
+  files = Catalog::Import::FindFiles.files(event)
+  files.each do |file|
+    begin
+      mover = Catalog::Import::MoveFile.new(source: file, base: "/mnt/music")
+      if mover.relocate
+        Catalog::Import::ImportFile.add(mover.new_path)
+      end
+    rescue Exception => e
+      $stdout.puts e
+    end
+  end
 end
