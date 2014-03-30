1480067bf912b317c091e620f6883daeed1f41eb
CulleyS <culley.smith@gmail.com>
2014-03-29 21:15:41 -0500

    integrate MultimediaTools::Metadata::Ffprobe into MultimediaTools::Metadata::Read

--- lib/multimedia_tools/metadata/read.rb
+++ lib/multimedia_tools/metadata/read.rb
@@ -1,4 +1,5 @@
 require 'taglib'
+require 'multimedia_tools/metadata/ffprobe'
 
 module MultimediaTools
   module Metadata
@@ -11,27 +12,34 @@ module MultimediaTools
 
       def tags
         TagLib::FileRef.open(source_file) do |fileref|
-          tag = fileref.tag
-          properties = fileref.audio_properties
+          tag = fileref.tag || ffprobe.tags
+          properties = fileref.audio_properties || ffprobe.properties
 
           new_file = {
             album: tag.album,
             artist: tag.artist,
-            bit_rate: properties.bitrate,
-            date: tag.year,
-            duration: properties.length,
+            bit_rate: properties.bitrate || properties.bit_rate.to_i,
+            date: tag.year || tag.date.to_i,
+            duration: properties.length || properties.duration.to_i,
             filename: source_file,
             filesize: File.size(source_file),
             format: File.extname(source_file)[1..-1],
             genre: tag.genre,
             title: tag.title,
-            track: tag.track
+            track: tag.track.to_i
           }
         end
       end
 
       def self.tags(source_file)
         new(source_file).tags
+      end
+
+      private
+      def ffprobe
+        # TagLib has problems with some tags, but is about 6 times faster than Ffprobe
+        # Ffprobe serves as a suitable backup when TagLib fails to read tags
+        @probe ||= Ffprobe.new(source_file)
       end
     end
   end
--- spec/lib/multimedia_tools/metadata/read_spec.rb
+++ spec/lib/multimedia_tools/metadata/read_spec.rb
@@ -1,23 +1,82 @@
-require 'multimedia_tools/metadata/read'
+require 'spec_helper'
 
 describe MultimediaTools::Metadata::Read do
   subject { MultimediaTools::Metadata::Read }
 
-  let(:file) { "spec/fixtures/17 More Than A Mouthful.mp3" }
-
   it "has tags for a file" do
-    tags = MultimediaTools::Metadata::Read.tags(file)
+    tags = double(album: "Album", artist: "Artist",
+                  year: 1975, genre: "Genre", title: "Title",
+                  track: 1)
+
+    properties = double(length: 123, bitrate: 234)
+    fileref = double(tag: tags, audio_properties: properties)
+
+    expect(TagLib::FileRef).
+      to receive(:open).
+      with("fake.txt").
+      and_yield(fileref)
+
+    expect(File).
+      to receive(:size).
+      with("fake.txt").
+      and_return(456)
+
+    tags = MultimediaTools::Metadata::Read.new("fake.txt").tags
+
     expect(tags).to eq({
-      album: "Nu.wav Hallucinations",
-      artist: "Nmesh",
-      bit_rate: 319,
-      date: 2013,
-      duration: 46,
-      filename: "spec/fixtures/17 More Than A Mouthful.mp3",
-      filesize: 1856841,
-      format: "mp3",
-      genre: "Vaporwave",
-      title: "More Than A Mouthful",
-      track: 17})
+      album: "Album",
+      artist: "Artist",
+      bit_rate: 234,
+      date: 1975,
+      duration: 123,
+      filename: "fake.txt",
+      filesize: 456,
+      format: "txt",
+      genre: "Genre",
+      title: "Title",
+      track: 1})
+  end
+
+  it "uses Ffprobe as a backup if TagLib fails" do
+    fileref = double(tag: nil, audio_properties: nil)
+
+    tags = double(album: "Album", artist: "Artist",
+                  date: "1975", genre: "Genre", title: "Title",
+                  track: "1", year: nil)
+
+    properties = double(bitrate: nil, duration: "123",
+                        bit_rate: "234", length: nil)
+
+    ffprobe = double(tags: tags, properties: properties)
+
+    expect(TagLib::FileRef).
+      to receive(:open).
+      with("fake.txt").
+      and_yield(fileref)
+
+    expect(MultimediaTools::Metadata::Ffprobe).
+      to receive(:new).
+      with("fake.txt").
+      and_return(ffprobe)
+
+    expect(File).
+      to receive(:size).
+      with("fake.txt").
+      and_return(456)
+
+    read_tags = MultimediaTools::Metadata::Read.tags("fake.txt")
+
+    expect(read_tags).to eq({
+      album: "Album",
+      artist: "Artist",
+      bit_rate: 234,
+      date: 1975,
+      duration: 123,
+      filename: "fake.txt",
+      filesize: 456,
+      format: "txt",
+      genre: "Genre",
+      title: "Title",
+      track: 1})
   end
 end
