e2308c7ecb747619bb17ec068c37828312db4122
CulleyS <culley.smith@gmail.com>
2014-03-30 20:05:34 -0500

    add Artist.names convenience method for returning all Artist names in alphabetical order

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -10,6 +10,10 @@ class Artist < ActiveRecord::Base
   # validations
   validates :name, presence: true, uniqueness: true
 
+  def self.names
+    all.map(&:name).sort
+  end
+
   def albums_by_date
     albums.sort_by(&:year)
   end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Artist do
+  let(:artist) { Artist.create(name: "David Bowie") }
+
   context "associations" do
     it { should have_many(:albums).dependent(:destroy) }
     it { should have_many(:songs) }
@@ -11,9 +13,16 @@ describe Artist do
     it { should validate_uniqueness_of(:name) }
   end
 
+  context "#names" do
+    it "returns all artist names in alphabetical order" do
+      artist
+      Artist.create(name: "Adam Ant")
+      expect(Artist.names).to eq(["Adam Ant", "David Bowie"])
+    end
+  end
+
   context "#albums_by_date" do
     it "sorts albums by date" do
-      artist = Artist.create(name: "David Bowie")
       year_one = ReleaseDate.create(year: 1973)
       year_two = ReleaseDate.create(year: 1977)
 
