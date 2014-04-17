529d55e1a84c118cb68fb67624322e50ba4112dd
CulleyS <culley.smith@gmail.com>
2014-04-16 21:50:27 -0500

    add Catalog::DatabaseTools::CatalogRecord

---
+++ lib/catalog/database_tools/catalog_record.rb
@@ -0,0 +1,43 @@
+module Catalog
+  module DatabaseTools
+    class CatalogRecord
+      attr_reader :tags
+
+      def initialize(tags)
+        @tags = tags
+      end
+
+      def create
+        updateable = Song.find(song.id)
+        updateable.song_file = song_file
+        updateable.save
+      end
+      
+      private
+      def artist
+        Catalog::DatabaseTools::ArtistRecord.add(name: tags[:artist])
+      end
+
+      def genre
+        Catalog::DatabaseTools::GenreRecord.add(name: tags[:genre])
+      end
+
+      def release_date
+        Catalog::DatabaseTools::ReleaseDateRecord.add(year: tags[:date])
+      end
+
+      def album
+        Catalog::DatabaseTools::AlbumRecord.add(title: tags[:album], artist: artist,
+                        genre: genre, release_date: release_date)
+      end
+
+      def song_file
+        Catalog::DatabaseTools::SongFileRecord.add(tags, fileable_id: song.id)
+      end
+
+      def song
+        @created_song ||= Catalog::DatabaseTools::SongRecord.add(tags, album)
+      end
+    end
+  end
+end
---
+++ spec/lib/catalog/database_tools/catalog_record_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe Catalog::DatabaseTools::CatalogRecord do
+  subject { Catalog::DatabaseTools::CatalogRecord }
+
+  let(:filepath) { "spec/fixtures/17 More Than A Mouthful.mp3" }
+  let(:tags) { MultimediaTools::Metadata::Read.tags(filepath) }
+
+  it "creates Artist, Genre, Album, Song, SongFile for the import file" do
+    subject.new(tags).create
+    artist = Artist.last
+    album = Album.last
+    genre = Genre.last
+    release_date = ReleaseDate.last
+    song = Song.last
+    song_file = SongFile.last
+
+    expect(artist.name).to eq("Nmesh")
+
+    expect(genre.name).to eq("Vaporwave")
+
+    expect(release_date.year).to eq(2013)
+
+    expect(album.title).to eq("Nu.wav Hallucinations")
+    expect(album.artist).to eq(artist)
+    expect(album.genre).to eq(genre)
+    expect(album.release_date).to eq(release_date)
+
+    expect(song.title).to eq("More Than A Mouthful")
+    expect(song.artist).to eq(artist)
+    expect(song.album).to eq(album)
+    expect(song.song_file).to eq(song_file)
+
+    expect(song_file.bit_rate).to eq(319)
+    expect(song_file.duration).to eq(46.0)
+    expect(song_file.filename).to eq(filepath)
+    expect(song_file.filesize).to eq(1856841)
+    expect(song_file.format).to eq("mp3")
+    expect(song_file.mtime).to eq("2014-03-18 02:24:57.834328000 +0000")
+  end
+end
