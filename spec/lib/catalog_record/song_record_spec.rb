84f0876b6ff7181621b27c35ac97e0bb6f135e78
CulleyS <culley.smith@gmail.com>
2014-04-20 17:36:02 -0500

    remove spec_helper dependency from song_record_spec

--- spec/lib/catalog_record/song_record_spec.rb
+++ spec/lib/catalog_record/song_record_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'catalog_record/song_record'
 
 describe CatalogRecord::SongRecord do
   subject { CatalogRecord::SongRecord }
@@ -7,9 +7,9 @@ describe CatalogRecord::SongRecord do
   let(:song_model) { Class.new} 
   let(:album) { double(artist: "foo") }
 
-  it "adds new Song" do
-    stub_const("SongModel", song_model)
+  before { stub_const("Song", song_model) }
 
+  it "adds new Song" do
     expect(song_model).
       to receive(:find_or_create_by).
       with(title: "Warszawa", artist: "foo", album: album, track: 8)
