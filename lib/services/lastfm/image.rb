b601c6cc7e0fcbe735f9a23e362289be6d7e7744
CulleyS <culley.smith@gmail.com>
2014-05-01 21:29:05 -0500

    add Services::Lastfm::Image superclass

---
+++ lib/services/lastfm/image.rb
@@ -0,0 +1,54 @@
+require 'services/lastfm/convert'
+require 'services/lastfm/download'
+require 'services/lastfm/uri'
+require 'active_support/inflector'
+
+module Services
+  module Lastfm
+    class Image
+      attr_reader :audio_file
+
+      def initialize(audio_file)
+        @audio_file = audio_file
+      end
+
+      def download
+        downloader.save and converter.convert
+        converter.new_path
+      end
+
+      private
+      def api_method
+        self.class.name.demodulize.downcase.sub("image", "")
+      end
+
+      def converter
+        @_converter ||= Convert.new(downloader.location)
+      end
+
+      def downloader
+        @_downloader ||= Download.new(tags: tags, url: download_url)
+      end
+
+      def download_url
+        uri[api_method]["image"].last[lastfm_image_key]
+      end
+
+      def escaped_artist
+        escape(tags.artist)
+      end
+
+      def escape(query)
+        query.gsub("\s","+")
+      end
+
+      def lastfm_image_key
+        "#text"
+      end
+
+      def tags
+        @_tags ||= audio_file.tags
+      end
+    end
+  end
+end
