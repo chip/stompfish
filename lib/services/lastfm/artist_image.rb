622cb81dedb6891fccf3533fc82853088a180f9b
CulleyS <culley.smith@gmail.com>
2014-05-01 21:29:38 -0500

    refactor Services::Lastfm::ArtistImage to inherit from Services::Lastfm::Image

--- lib/services/lastfm/artist_image.rb
+++ lib/services/lastfm/artist_image.rb
@@ -1,48 +1,11 @@
-require 'services/lastfm/convert'
-require 'services/lastfm/download'
-require 'services/lastfm/uri'
+require 'services/lastfm/image'
 
 module Services
   module Lastfm
-    class ArtistImage
-      attr_reader :audio_file
-
-      def initialize(audio_file)
-        @audio_file = audio_file
-      end
-
-      def download
-        downloader.save and converter.convert
-        converter.new_path
-      end
-
+    class ArtistImage < Image
       private
-      def converter
-        @_converter ||= Convert.new(downloader.location)
-      end
-
-      def downloader
-        @_downloader ||= Download.new(tags: tags, url: download_url)
-      end
-
       def uri
         @_uri ||= Uri.new("artist.getinfo&artist=#{escaped_artist}").get
-      end
-
-      def escaped_artist
-        tags.artist.gsub("\s","+")
-      end
-
-      def tags
-        @_tags ||= audio_file.tags
-      end
-
-      def download_url
-        uri["artist"]["image"].last[lastfm_image_key]
-      end
-
-      def lastfm_image_key
-        "#text"
       end
     end
   end
