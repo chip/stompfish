c687c9daec4e918193edbc1a37b075c3ed9d8041
CulleyS <culley.smith@gmail.com>
2014-05-18 11:48:12 -0500

    add Playlist#songs to return song objects

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,4 +4,8 @@ class Playlist < ActiveRecord::Base
   # validations
   validates_presence_of :title
   validates :song_ids, only_integer_values: true
+
+  def songs
+    song_ids.map { |id| Song.find(id) }
+  end
 end
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -10,4 +10,15 @@ describe Playlist do
       expect(playlist.errors.messages). to eq({song_ids: ["must be integer values"]})
     end
   end
+
+  it "has songs" do
+    artist = Artist.create(name: "Artist")
+    album = Album.create(title: "Album", artist: artist)
+    song = Song.create(title: "Song", artist: artist, album: album)
+    playlist = Playlist.create(title: "Playlist")
+
+    playlist.song_ids = [song.id]
+    playlist.save
+    expect(playlist.songs).to eq([song])
+  end
 end
