b4f7c9a35ed529ba2cd2324dd697bd67306e0f1c
CulleyS <culley.smith@gmail.com>
2014-05-18 11:47:12 -0500

    use songs resource url in Playlists#songs response

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,4 +1,13 @@
 class PlaylistSerializer < ActiveModel::Serializer
   # attributes
-  attributes :id, :title, :song_ids
+  attributes :id, :title, :songs
+
+  def songs
+    object.songs.map { |song| url_for_resource(song) }
+  end
+
+  private
+  def url_for_resource(resource)
+    url_for(host: ApplicationSettings["host_path"], controller: "songs", action: "show", id: resource)
+  end
 end
