dfe84dab4bede07c0967d119f16eb47bb82001de
CulleyS <culley.smith@gmail.com>
2014-05-19 22:11:13 -0500

    remove keyword arguments from private methods in PlaylistManager

--- lib/playlist_manager.rb
+++ lib/playlist_manager.rb
@@ -10,14 +10,14 @@ class PlaylistManager
 
   def add(song: song, position: position)
     @position = position
-    update_song_ids(song: song) do
+    update_song_ids(song) do
       song_ids.insert(position.to_i, song.id).compact!
     end
   end
 
   def delete(song: song)
     @position = :ignore
-    update_song_ids(song: song) { song_ids.delete(song.id) }
+    update_song_ids(song) { song_ids.delete(song.id) }
   end
 
   def runtime
@@ -38,15 +38,15 @@ class PlaylistManager
     @_songs ||= playlist.songs
   end
 
-  def update_song_ids(song: song, &block)
-    validate_params(song: song)
+  def update_song_ids(song, &block)
+    validate_params(song)
     return if errors.any?
     playlist.song_ids_will_change!
     yield if block_given?
     playlist.save
   end
 
-  def validate_params(song: song)
+  def validate_params(song)
     song.nil? and errors[:song] = "Resource Not Found."
     position.nil? or position.to_s.empty? and errors[:position] = "can't be blank"
   end
